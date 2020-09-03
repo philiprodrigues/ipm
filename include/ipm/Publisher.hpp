bd6705a43dc2b80779babdc41102091772ef3ecb
Eric Flumerfelt <eflumerf@fnal.gov>
2020-09-03 09:15:33 -0500

    Adding basic structure with Phil's proposed class names.

---
+++ include/ipm/Publisher.hpp
@@ -0,0 +1,21 @@
+#ifndef IPM_INCLUDE_IPM_PUBLISHER_HPP_
+#define IPM_INCLUDE_IPM_PUBLISHER_HPP_
+
+/**
+ *
+ * @file Publisher.hpp Publisher messaging role class declarations
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+namespace dunedaq {
+
+namespace ipm {
+class Publisher
+{};
+} // namespace ipm
+} // namespace dunedaq
+
+#endif // IPM_INCLUDE_IPM_PUBLISHER_HPP_
---
+++ include/ipm/Subscriber.hpp
@@ -0,0 +1,21 @@
+#ifndef IPM_INCLUDE_IPM_SUBSCRIBER_HPP_
+#define IPM_INCLUDE_IPM_SUBSCRIBER_HPP_
+
+/**
+ *
+ * @file Subscriber.hpp Subscriber messaging role class declarations
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+namespace dunedaq {
+
+namespace ipm {
+class Subscriber
+{};
+} // namespace ipm
+} // namespace dunedaq
+
+#endif // IPM_INCLUDE_IPM_SUBSCRIBER_HPP_
